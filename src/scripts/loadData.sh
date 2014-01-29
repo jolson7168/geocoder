0254b3b19ce0fecf0c36ebdc253e36077c5e9a34
Joe Olson <jo6971@nododos.com>
2014-01-29 03:37:02 +0000

    intial add

---
+++ src/scripts/loadData.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#as OS user oracle...
+
+echo "CREATE TABLESPACE geo NOLOGGING DATAFILE '/u01/oradata/xe/geo1.dbf' SIZE 250M,'/u01/oradata/xe/geo2.dbf' SIZE 250M,'/u01/oradata/xe/geo3.dbf' SIZE 250M,'/u01/oradata/xe/geo4.dbf' SIZE 250M autoextend on;" | sqlplus / as sysdba
+echo 'create user geo identified by geo default tablespace geo;' | sqlplus / as sysdba
+echo 'grant all privileges to geo;' | sqlplus / as sysdba
+
+echo '@/u01/git/geocoder/src/sql/CreateSchema.sql;' |sqlplus geo/geo
+/u01/git/geocoder/src/scripts/importcountries.sh geo geo
+/u01/git/geocoder/src/scripts/importstates.sh geo geo
